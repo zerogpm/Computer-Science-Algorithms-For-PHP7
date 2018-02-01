efc0f7b56bd1e3efa6dc544201d2631c4b676586
Chris Su <zerogpm@gmail.com>
2018-02-01 14:56:41 -0800

    added reversed String

---
+++ String/reversedString.php
@@ -0,0 +1,19 @@
+<?php
+
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+$string = 'hello';
+
+$reverse = '';
+
+foreach (str_split($string) as $value) {
+    $reverse = $value . $reverse;
+}
+
+return $reverse;
